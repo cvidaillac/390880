85bce66ab50d7b89317c67bc68d3c9f69b5d2e71
Christophe Vidaillac <christophe.vidaillac@bonitasoft.com>
2024-06-27 14:18:48 -0400

    test

--- restAPIExtensions/pop3PLyreGetEnergiesRestAPI/src/main/java/com/sgdf/rest/api/exception/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.sgdf.rest.api.exception;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
